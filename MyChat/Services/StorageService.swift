3127f17a85f6e0745f0364f2c10b5c9cd6322e51
Oleg Kanatov <olegkanatov7@gmail.com>
2021-12-21 15:33:06 +0300

    add Save Image with Firebase Storage

---
+++ MyChat/Services/StorageService.swift
@@ -0,0 +1,49 @@
+//
+//  StorageService.swift
+//  MyChat
+//
+//  Created by Oleg Kanatov on 21.12.21.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseStorage
+
+class StorageService {
+    
+    static var shared = StorageService()
+    
+    let storageRef = Storage.storage().reference()
+    
+    private var avatarsRef: StorageReference {
+        return storageRef.child("avatars")
+    }
+    
+    private var currentUserId: String {
+        return Auth.auth().currentUser!.uid
+    }
+    
+    func upload(photo: UIImage, completion: @escaping (Result<URL, Error>) -> Void) {
+        
+        guard let scaledImage = photo.scaledToSafeUploadSize,
+              let imageData = scaledImage.jpegData(compressionQuality: 0.4) else { return }
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        avatarsRef.child(currentUserId).putData(imageData, metadata: metadata) { metadata, error in
+            guard let _ = metadata else {
+                completion(.failure(error!))
+                return
+            }
+            
+            self.avatarsRef.child(self.currentUserId).downloadURL { url, error in
+                guard let downloadURL = url else {
+                    completion(.failure(error!))
+                    return
+                }
+                completion(.success(downloadURL))
+            }
+        }
+    }
+}
