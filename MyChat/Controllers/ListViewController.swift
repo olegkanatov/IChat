afa6b0dbbe516ad121518cd6faf077b63bb8c95e
Oleg Kanatov <olegkanatov7@gmail.com>
2021-12-24 15:18:54 +0300

    add MessageKit

--- MyChat/Controllers/ListViewController.swift
+++ MyChat/Controllers/ListViewController.swift
@@ -244,7 +244,9 @@ extension ListViewController: UICollectionViewDelegate {
             chatRequestVC.delegate = self
             self.present(chatRequestVC, animated: true, completion: nil)
         case .activeChats:
-            print(indexPath)
+            let chatsVC = ChatsViewController(user: currentUser, chat: chat)
+            navigationController?.pushViewController(chatsVC, animated: true)
+            
         }
     }
 }
